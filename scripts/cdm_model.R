ecba5222a58c9e53cbae079901cb438e37d84b1e
Nobuaki Mizumoto (水元 惟暁) <71577022+nobuaki-mzmt@users.noreply.github.com>
2023-06-27 21:13:03 +0900

    added simulation results

--- scripts/cdm_model.R
+++ scripts/cdm_model.R
@@ -1,18 +1,41 @@
-# Simple model for collective decision making
+# Simple simulation model for collective decision making
 # based on Jeanson et al 2012
 # N. Mizumoto
 
-param_a  <- 0.01    # state change speed
-param_k  <- c(2, 1) # propensity of the state (non-active vs active)
-param_r  <- 2       # strength of positive feedback
-sim_time <- 1000
-sim_rep  <- 1
+rm(list = ls())
 
-num_ind_vec  <- c(10,20,50,100,200,500,1000,2000,5000,10000)
+#------------------------------------------------------------------------------#
+{
+  library(plyr)
+  library(ggplot2)
+  library(viridis)
+}
+#------------------------------------------------------------------------------#
 
-# Simulation
-SwarmingActivationSim <- function(num_ind, active_prop){
-  ind_X <- rep(c(1, 2), c((num_ind-num_ind*active_prop), (num_ind*active_prop)))
+#------------------------------------------------------------------------------#
+{
+  param <- list(a = 0.01, k  = c(2, 1), r  = 2)
+  # a: state change speed;
+  # k: propensity of the state (non-active vs active)
+  # r: strength of positive feedback
+  
+  sim_time     <- 1000
+  sim_rep      <- 100
+  num_ind_vec  <- c(10,100,1000)
+  prop_vec     <- (0:10)/10
+  RunSimulations(sim_rep, param)
+  Sim_output()
+}
+#------------------------------------------------------------------------------#
+
+#------------------------------------------------------------------------------#
+# Simulation function
+#------------------------------------------------------------------------------#
+SwarmingActivationSim <- function(num_ind, active_prop, param=param){
+  param_a = param$a
+  param_k = param$k
+  param_r = param$r
+  ind_X <- rep(c(1, 2), c((num_ind-round(num_ind*active_prop)), round(num_ind*active_prop)))
   X     <- matrix(num_ind/2, sim_time, 2)
   for(i_time in 2:sim_time){
     for(i_ind in 1:num_ind){
@@ -27,32 +50,60 @@ SwarmingActivationSim <- function(num_ind, active_prop){
     X[i_time, 1] = sum(ind_X == 1)
     X[i_time, 2] = sum(ind_X == 2)
   }
-  
   return(X[, 1] / num_ind)
 }
+#------------------------------------------------------------------------------#
 
-# Show results
-res_sum = NULL
-for(iter_num_ind in num_ind_vec){
-  for(iter_prop in 0:10){
-    for(iter in 1:sim_rep){
-      print(paste(iter_num_ind, iter_prop, iter))
-      res = SwarmingActivationSim(iter_num_ind, iter_prop/10)
-      res_sum = c(res_sum, res[sim_time])
+#------------------------------------------------------------------------------#
+# Run simualtions
+#------------------------------------------------------------------------------#
+RunSimulations <- function(sim_rep, param){
+  res_sum = NULL
+  for(iter_num_ind in num_ind_vec){
+    for(iter_prop in prop_vec){
+      for(iter in 1:sim_rep){
+        print(paste(iter_num_ind, iter_prop, iter))
+        res = SwarmingActivationSim(iter_num_ind, iter_prop, param = param)
+        res_sum = c(res_sum, res[sim_time])
+      }
     }
   }
+  
+  df_sim_res = data.frame(
+    num_ind = rep(num_ind_vec, each = 11*sim_rep),
+    active_prop = rep(rep(prop_vec, each=sim_rep), length(num_ind_vec)),
+    inactive = res_sum
+  )
+  save(df_sim_res, file = "data/df_sim_res.rda")
 }
+#------------------------------------------------------------------------------#
 
-df = data.frame(
-  num_ind = rep(num_ind_vec, each = 11*sim_rep),
-  inactive_prop = rep(0:10/10, sim_rep*length(num_ind_vec)),
-  inactive = res_sum
-)
-library(MASS)
-image.plot(matrix(res_sum, 11, 10))
-ggplot(df, aes(as.factor(inactive_prop), as.factor(num_ind), fill= inactive)) + 
-  geom_tile()+
-  scale_fill_viridis()
-res_sum
-library(viridis)
-SwarmingActivationSim(10, 0.6)
+#------------------------------------------------------------------------------#
+# Output
+#------------------------------------------------------------------------------#
+Sim_output <- function(){
+  load("data/df_sim_res.rda")
+  
+  df_sim_sum <- ddply(df_sim_res, .(active_prop, num_ind), summarize, 
+                      inactive=mean(inactive))
+  df_sim_sum <- cbind(df_sim_sum,  
+                      inactive_error=as.vector(tapply(df_sim_res$inactive, df_sim_res[,1:2], sd)))
+  
+  ggplot(df_sim_res, aes(x=active_prop, y=1-inactive, col=log10(num_ind)))+
+    #scale_color_viridis(discrete=T, end=0, begin = 1) +
+    geom_jitter(position = position_jitterdodge(dodge.width=.05, jitter.width = .02),
+                alpha=0.1)+
+    theme_classic() +
+    theme(legend.position = "none")+
+    geom_point(data = df_sim_sum, aes(x=active_prop, y=1-inactive, 
+                                      group=as.factor(num_ind), col=log10(num_ind)),
+               position = position_dodge(.05))+
+    geom_path(data = df_sim_sum, aes(x=active_prop, y=1-inactive,
+                                     group=as.factor(num_ind), col=log10(num_ind)),
+              position = position_dodge(.05), )+
+    geom_abline(linetype = "dashed", col="grey", )+
+    xlab("Propotion of active alates at the initial state (~temperature activation)")+
+    ylab("Propotion of active alates at the end (~dispersed individuals)")
+  ggsave("output/model_resutls.pdf", width=5, height=3)
+}
+#------------------------------------------------------------------------------#
